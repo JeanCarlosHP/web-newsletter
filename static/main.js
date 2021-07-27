919ebb366a4c84125bddcdbf9356008867392b77
Jean Carlos <jeanchpaula@gmail.com>
2021-07-27 19:15:31 -0300

    acesso ao json com o conteudo e data

---
+++ static/main.js
@@ -0,0 +1,15 @@
+﻿const date = document.querySelector('header>p>span')
+const content = document.querySelector('#content')
+
+getData()
+
+async function getData() {
+  const response = await fetch('http://127.0.0.1:8000/json')
+  const responseJson = await response.json()
+
+  localStorage.setItem('data', JSON.stringify(responseJson))
+  const contentJson = JSON.parse(localStorage.getItem('data'))
+
+  date.innerHTML += contentJson.date
+  content.innerHTML += contentJson.content
+}
