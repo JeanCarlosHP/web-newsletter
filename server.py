a5524346f37ad1b7dab55859a5328930e64aa846
Jean Carlos <jeanchpaula@gmail.com>
2021-07-27 19:17:50 -0300

    rota do html principal e do arquivo json

---
+++ server.py
@@ -0,0 +1,33 @@
+﻿from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from backend.extract_content import EmailContent
+
+app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/', response_class=HTMLResponse)
+async def main(request: Request):
+
+    await json()
+
+    return templates.TemplateResponse(
+        'index.html',
+        {
+            'request': request
+        })
+
+
+@app.get('/json')
+async def json():
+    emailContent = EmailContent()
+
+    content = emailContent.extractContent()
+
+    return content
