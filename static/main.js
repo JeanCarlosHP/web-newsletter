b6c5b41feb6e31722b0d377666b8be5ed9bfda55
Jean Carlos <jeanchpaula@gmail.com>
2021-07-28 19:19:39 -0300

    função getData para função anônima data

--- static/main.js
+++ static/main.js
@@ -1,11 +1,9 @@
 ﻿const date = document.querySelector('header>p>span')
 const content = document.querySelector('#content')
 
-getData()
-
-async function getData() {
-  const response = await fetch('http://127.0.0.1:8000/json')
-  const responseJson = await response.json()
+const data = () => {
+  const response = fetch('http://127.0.0.1:8000/json')
+  const responseJson = response.json()
 
   localStorage.setItem('data', JSON.stringify(responseJson))
   const contentJson = JSON.parse(localStorage.getItem('data'))
